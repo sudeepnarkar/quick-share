7dcc3525e9c3526a6a5f1531bc0df95226eb44d7
Sudeep Narkar <narkarsudeep1@gmail.com>
2016-01-31 20:43:04 -0500

    Modified pin model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-	attr_accessor :image_file_name
+	attr_accessor :image_file_name, :image_content_type, :image_file_size, :image_updated_at
 	belongs_to :user
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 	
