8717f95074560bf13ff713bfb72ae62be48dc02c
Sudeep Narkar <narkarsudeep1@gmail.com>
2016-02-01 18:37:39 -0500

    Added changes to production.rb and configure heroku config envs for aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,13 +83,13 @@ Rails.application.configure do
    config.action_mailer.default_url_options = { host: 'www.quick-share.herokuapp.com', port: 3000 }
 
 #Sets Paperclip to upload images on Amazon S3
-     config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+        :bucket => ENV['AWS_BUCKET'],
+        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
     }
-  }
 
-end
+
