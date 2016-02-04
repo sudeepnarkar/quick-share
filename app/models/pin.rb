4bc68fcb549f0a42f0d7d2e04c96343c3d776d11
Sudeep Narkar <narkarsudeep1@gmail.com>
2016-02-04 04:17:45 -0500

    made changes to production.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-
+	#attr_accessor :image_file_name
 	belongs_to :user
 	has_attached_file :image , :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
